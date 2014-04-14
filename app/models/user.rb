f4ae75372f9970c948b20a33beaa8fccad94381a
Ram Iyer <ramky.iyer@yahoo.com>
2014-04-14 15:53:25 -0700

    completed user can reorder videos in the queue

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ActiveRecord::Base
   has_many :reviews
-  has_many :queue_items
+  has_many :queue_items, order: :position
   has_secure_password validations: false
   validates_presence_of :email, :password, :full_name
+  validates_uniqueness_of :email
 end
+
