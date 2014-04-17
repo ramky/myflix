056e908fffa278afa8f7f60b62cc7478347bc5eb
Ram Iyer <ramky.iyer@yahoo.com>
2014-04-17 13:58:18 -0700

    adding macros and shared_examples

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -8,7 +8,7 @@ describe SessionsController do
     end
 
     it "redirects to the home page for authenticated users" do
-      session[:user_id] = Fabricate(:user).id
+      set_current_user()
       get :new
       expect(response).to redirect_to home_path
     end
--- spec/controllers/videos_controller_spec.rb
+++ spec/controllers/videos_controller_spec.rb
@@ -3,14 +3,14 @@ require 'spec_helper'
 describe VideosController do
   describe "GET show" do
     it "sets @video for authenticated users" do
-      session[:user_id] = Fabricate(:user).id
+      set_current_user()
       video = Fabricate(:video)
       get :show, id: video.id
       expect(assigns(:video)).to eq(video) 
     end      
 
     it "sets @reviews for authenticated users" do
-      session[:user_id] = Fabricate(:user).id
+      set_current_user()
       video   =  Fabricate(:video)
       review1 = Fabricate(:review, video: video)
       review2 = Fabricate(:review, video: video)
@@ -18,10 +18,9 @@ describe VideosController do
       expect(assigns(:reviews)).to match_array([review1, review2])
     end
 
-    it "redirects the user to the sign in page for unauthenticated users" do
+    it_behaves_like "requires sign in" do
       video = Fabricate(:video)
-      get :show, id: video.id
-      expect(response).to redirect_to sign_in_path
+      let(:action) { get :show, id: video.id }
     end
   end
 
